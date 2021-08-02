f0fd22f1f66e51e89ea64dc7d5f5ae92aeb9c312
Michel Almeida <85575854+michelalmeida2812@users.noreply.github.com>
2021-08-02 19:46:46 -0300

    Add files via upload
    Um projeto com loop.

---
+++ Interacao.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Interacao {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Digite seu nome");
+        System.out.println("Ola " + name);
+        }
+    }
